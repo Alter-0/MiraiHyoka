20287b367bdf41a216bebe491898d868fbd2cee1
lim8639 <863978160@qq.com>
2020-12-19 14:37:48 +0800

    更新css

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MiraiHyoka</title>
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="">
+    <script src="js/jquery.js">
+    </script>
 </head>
 <body>
 <h1>电子商务系二级项目</h1>
