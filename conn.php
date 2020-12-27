30d73f50ba2cbf005272883d3ff6cf0bd395383b
lim8639 <863978160@qq.com>
2020-12-27 21:06:38 +0800

    完整发布

--- conn.php
+++ conn.php
@@ -4,4 +4,10 @@ $conn = mysqli_connect("47.115.15.18",
     "miraihyoka") or die("数据库连接失败");
 mysqli_query($conn,'set names utf8');
 
+
+
+//$conn = mysqli_connect("47.115.15.18",
+//    "wangyesheji", "e7BLUzfQv69wXybN",
+//    "miraihyoka") or die("数据库连接失败");
+//mysqli_query($conn,'set names utf8');
 ?>
