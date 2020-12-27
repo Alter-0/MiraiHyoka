b1a21492a0f67a2db488aff9086e440ec65269ce
BlueberryQAQ <50608727+BlueberryQAQ@users.noreply.github.com>
2020-12-27 23:01:16 +0800

    12.27 修正

--- index.php
+++ index.php
@@ -16,6 +16,41 @@
     <script src="js/jquery.js"></script>
     <script src="css/bootstrap/js/bootstrap.min.js"></script>
     <script src="js/index.js"></script>
+    <script>
+        //导入网站通用文件@blueberry
+        //设置站点图标
+        document.write('<link rel="shortcut icon" href="image/favicon.ico">');
+        //通用样式
+        document.write('<link rel="stylesheet" type="text/css" href="css/default.css">');
+        document.write('<link rel="stylesheet" type="text/css" href="css/loading.css">');
+    </script>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/loading.js"></script>
+    <script src="js/sakura.js"></script>
+    <script src="js/mouse_click.js"></script>
+    <script src="js/change_title.js"></script>
+    <script src="live2d/assets/jquery.min.js?v=3.3.1"></script>
+    <script src="live2d/assets/jquery-ui.min.js?v=1.12.1"></script>
+    <script>
+        try {
+            $("<link>").attr({href: "live2d/assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+            $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
+            $.ajax({url: 'live2d/assets/waifu-tips.js?v=1.4.2',dataType:"script", cache: true, async: false});
+            $.ajax({url: 'live2d/assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
+            /* 可直接修改部分参数 */
+            // live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
+            // live2d_settings['modelId'] = 1;                  // 默认模型 ID
+            // live2d_settings['modelTexturesId'] = 0;          // 默认材质 ID
+            // live2d_settings['modelStorage'] = false;         // 不储存模型 ID
+            live2d_settings['modelAPI']             = 'api/';
+            /* 在 initModel 前添加 */
+            initModel('live2d/assets/waifu-tips.json?v=1.4.2');
+        } catch(err) { console.log('[Error] JQuery is not defined.') }
+    </script>
+<!--    <script src="live2d/assets/autoload.js?v=1.4.2"></script>-->
+    <script src="js/backtop.js"></script>
+
     <style>
         #showspider{
             height: 560px;
